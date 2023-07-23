e94901b8394f1e38397753aaf0d56570f34f8555
Tirth Patel <tirthpatelcse@gmail.com>
2023-07-23 14:52:09 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -10,5 +10,21 @@ gh repo create propeller-task --public --source=.
 # 3
 git checkout -b update_logo
 
+<<<<<<< HEAD
 # 6
 git add . && git commit -m "Update logo and link"
+=======
+# 4 Replaced the react spinning logo with given propeller logo 
+
+# 5 Replaced the href link with the given link
+
+# 6
+git add . && git commit -m "Update logo and link"
+
+# 7
+gh pr create --base master --fill
+
+# 8 
+gh pr merge
+
+>>>>>>> 0efc259 (Update command.sh)
